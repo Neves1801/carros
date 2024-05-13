43ca6de2df359fd628616a32facf0352c73a165e
Michelly Stephany Pfeifer Das Neves <128699395+Neves1801@users.noreply.github.com>
2024-05-13 11:49:27 -0300

    Add files via upload

---
+++ credit_payament.py
@@ -0,0 +1,18 @@
+from config import *
+from payament_times.in_time import *
+from payament_times.three_months import *
+from payament_times.six_months import *
+from payament_times.nine_months import *
+from payament_times.twelve_months import *
+
+def credit_payament(desired_car, time_option):
+    if(time_option == 1):
+        return (in_time(desired_car))
+    elif(time_option == 2):
+        return (three_months(desired_car))
+    elif(time_option == 3):
+        return(six_months(desired_car))
+    elif(time_option == 4):
+        return(nine_months(desired_car))
+    else:
+        return(twelve_months(desired_car))
---
+++ twelve_months.py
@@ -0,0 +1,3 @@
+from config import *
+def twelve_months (desired_car):
+    return f'Carro comprado parcelado em 12 meses no valor de {available_cars.get(desired_car)}'
